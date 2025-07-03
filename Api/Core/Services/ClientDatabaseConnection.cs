55b904dd4249ea9cb1daf97631f8e1a3ba8bb2dd
Jason van der Lee <jason@in-vista.nl>
2025-07-03 16:22:35 +0200

    change errorcode checking for re-try to use casted values in mysqlconection (#814)
    * change errorcode checking for re-try to use casted values in mysqlconnection
    
    * address concerns from coworkers

--- Api/Core/Services/ClientDatabaseConnection.cs
+++ Api/Core/Services/ClientDatabaseConnection.cs
@@ -306,7 +306,8 @@ namespace Api.Core.Services
                 }
 
                 // If we're not in a transaction, retry the query if it's a deadlock.
-                if (MySqlDatabaseConnection.MySqlErrorCodesToRetry.Contains(mySqlException.Number))
+                int errorCode = mySqlException.Number;
+                if (MySqlDatabaseConnection.MySqlErrorCodesToRetry.Contains(errorCode))
                 {
                     Thread.Sleep(gclSettings.TimeToWaitBeforeRetryingQueryInMilliseconds);
                     return await ExecuteAsync(query, retryCount + 1, useWritingConnectionIfAvailable, cleanUp);
