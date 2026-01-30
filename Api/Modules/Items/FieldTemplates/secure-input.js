494e62bfa7b9d9df278699a8a0afb08793da6b88
maicol <maicol@in-vista.nl>
2026-01-30 10:24:00 +0100

    Fixed the styling of the secure-input eye

--- Api/Modules/Items/FieldTemplates/secure-input.js
+++ Api/Modules/Items/FieldTemplates/secure-input.js
@@ -4,34 +4,21 @@
     let container = $("#container_{propertyIdWithSuffix}");
     let field = $("#field_{propertyIdWithSuffix}").change(window.dynamicItems.fields.onFieldValueChange.bind(window.dynamicItems.fields));
 
-    const eyeOpen  = '<i class="k-icon k-font-icon k-i-eye k-button-icon" style="font-size: 20px;"></i>';
-    const eyeClosed = '<i class="k-icon k-font-icon k-i-eye-slash k-button-icon" style="font-size: 20px;"></i>';
-    
-    const toggleText = container.find(".toggle-text");
+    const toggleVisibilityEyeElement = $("#password-toggle_{propertyIdWithSuffix}");
+
+    toggleVisibilityEyeElement.toggleClass("hidden", !canToggleSecureText);
+
     if (canToggleSecureText) {
-        toggleText
-            .removeClass("hidden")
-            .html(eyeClosed);
-        
-        toggleText.css(
-            {
-                "display": "flex",
-                "flex-wrap": "wrap",
-                "width": "40px",
-                "justify-content": "center"
+        toggleVisibilityEyeElement
+            .on("click", function () {
+                const isSecureText = field.attr("type") === "password";
+
+                field.attr("type", isSecureText ? "text" : "password");
+
+                toggleVisibilityEyeElement
+                    .toggleClass("icon-eye-visible", !isSecureText)
+                    .toggleClass("icon-eye-invisible", isSecureText);
             });
-        
-        toggleText.click(function () {
-            const current = field.attr("type");
-            
-            const isSecure = current === "password";
-            field.attr("type", isSecure ? "text" : "password");
-            
-            
-            toggleText.html(isSecure ? eyeOpen : eyeClosed);
-        });
-    } else{
-        toggleText.addClass("hidden")
     }
 
     {customScript}
